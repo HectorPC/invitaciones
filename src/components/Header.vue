45c96749cb34718984466059bdbb509de94cfd16
hpalmerocases <hector.palmerocases@soprasteria.com>
2018-10-23 15:38:43 +0200

    feat: header pc resolution

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,132 @@
 <template>
     <div id="header">
-        <h2>Header</h2>
+
+        <header class="main_header">
+
+            <div class="row">
+                <div class="content">
+                    <a class="logo" href="#">HPC</a>
+                    <nav>
+                        <ul>
+                            <li v-for="invitation in invitationTypes">
+                                <!-- <nav-link @click="selected = invitation.name" :class="{active : invitation.name == selected}">{{ invitation.name }}</nav-link> -->
+                                <nav-link :class="{active : invitation.name == selected}" @click.native="activeLink(invitation.name)">{{ invitation.name }}</nav-link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+        </header>
+
     </div>
 </template>
 
 <script>
+    import NavLink from './NavLink.vue'
     export default {
-        
+        data() {
+            return {
+                selected: '',
+                invitationTypes: [
+                    {name: 'Section01'},
+                    {name: 'Section02'},
+                    {name: 'Section03'},
+                    {name: 'Section04'}
+                ]
+            }
+        },
+        methods: {
+            activeLink(active) {
+                this.selected = active;
+            }
+        },
+        components: {
+            NavLink
+        }
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    * {
+        box-sizing: border-box;
+    }
 
+    #header {
+        height: 70px;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    .row {
+        width: 100%;
+        margin: 0 auto;
+        position: relative;
+        padding: 0 2%;
+    }
+
+    .content {
+        max-width: 940px;
+        width: 100%;
+        margin: 0 auto;
+        padding: 50px 2% 50px;
+    }
+
+    .main_header .row .content {
+        padding: 0;
+    }
+
+    .main_header {
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        max-height: 70px;
+        width: 100%;
+        padding-top: 17px;
+        overflow: hidden;
+        transition: all 0.3s;
+        padding-bottom: 6px;
+        /* font-family: "Oswald", sans-serif; */
+        background: black;
+    }
+
+    .logo {
+        width: 50px;
+        font-size: 25px;
+        color: #8f8f8f;
+        text-transform: uppercase;
+        float: left;
+        display: block;
+        margin-top: 0;
+        line-height: 1;
+        margin-bottom: 10px;
+    }
+
+    nav {
+        float: right;
+        width: 60%;
+    }
+
+    nav ul {
+        list-style: none;
+        overflow: hidden;
+        text-align: right;
+        float: right;
+    }
+
+    nav ul li {
+        display: inline-block;
+        margin-left: 35px;
+        line-height: 1.5;
+    }
+
+    .active {
+        background: grey;
+        color: white;
+        font-weight: bold;
+        border-radius: 3px;
+    }
+    
 </style>
---
+++ src/components/NavLink.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <a class="seventh before after" href="#">
+            <slot></slot>
+        </a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'NavLink'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    a, a:visited, a:hover, a:active {
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+        position: relative;
+        transition: 0.5s color ease;
+        text-decoration: none;
+        color: #81b3d2;
+        font-size: 18px;
+        padding: 0px 5px;
+    }
+
+    a:hover {
+        color: #81b3d2;
+    }
+
+    a.before:before, a.after:after {
+        content: "";
+        transition: 0.5s all ease;
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+        position: absolute;
+    }
+
+    a.before:before {
+        top: -0.25em;
+    }
+
+    a.after:after {
+        bottom: -0.25em;
+    }
+
+    a.before:before, a.after:after {
+        height: 5px;
+        width: 0px;
+        background: #316fb6;
+    }
+
+    a.seventh:before {
+        right: 0;
+    }
+
+    a.seventh:after {
+        left: 0;
+    }
+
+    a.before:hover:before, a.after:hover:after {
+        width: 100%;
+    }
+
+
+</style>
