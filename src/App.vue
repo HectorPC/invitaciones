350ce1cfb24b5a6d1b418faaa5c8d829413dd98a
hpalmerocases <hector.palmerocases@soprasteria.com>
2018-10-23 13:04:29 +0200

    feat: responsive header structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <app-header></app-header>
+    <app-header-responsive v-show="isVisibleResponsive"></app-header-responsive>
     <router-view></router-view>
     <app-footer></app-footer>
   </div>
@@ -8,16 +9,18 @@
 
 <script>
 import Header from './components/Header.vue'
+import HeaderResponsive from './components/HeaderResponsive.vue'
 import Footer from './components/Footer.vue'
 export default {
   name: 'App',
   data() {
     return {
-      
+      isVisibleResponsive: false
     }
   },
   components: {
     'app-header': Header,
+    'app-header-responsive': HeaderResponsive,
     'app-footer': Footer
   }
 }
