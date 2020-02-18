8a815bc930edb491a1c1b2d120f0ff78869fa03d
hpalmerocases <hector.palmerocases@soprasteria.com>
2020-02-18 14:07:13 +0100

    feat: style buttons invitation

--- src/components/ButtonGenerar.vue
+++ src/components/ButtonGenerar.vue
@@ -18,21 +18,18 @@
 	}
 
 	.myButton {
-		background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #44c767), color-stop(1, #5cbf2a));
-		background: -moz-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
-		background: -webkit-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
-		background: -o-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
-		background: -ms-linear-gradient(top, #44c767 5%, #5cbf2a 100%);
-		background: linear-gradient(to bottom, #44c767 5%, #5cbf2a 100%);
-		filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#44c767', endColorstr='#5cbf2a', GradientType=0);
-		background-color: #44c767;
+		background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #FCDA18), color-stop(1, #c1a91f));
+		background: -moz-linear-gradient(top, #FCDA18 5%, #c1a91f 100%);
+		background: -webkit-linear-gradient(top, #FCDA18 5%, #c1a91f 100%);
+		background: -o-linear-gradient(top, #FCDA18 5%, #c1a91f 100%);
+		background: -ms-linear-gradient(top, #FCDA18 5%, #c1a91f 100%);
+		background: linear-gradient(to bottom, #FCDA18 5%, #c1a91f 100%);
+		filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#FCDA18', endColorstr='#c1a91f', GradientType=0);
+		background-color: #FCDA18;
 		-moz-border-radius: 28px 5px 5px 28px;
-		;
 		-webkit-border-radius: 28px 5px 5px 28px;
-		;
 		border-radius: 28px 5px 5px 28px;
-		;
-		border: 3px solid #18ab29;
+		border: 3px solid #c1a91f;
 		display: inline-block;
 		cursor: pointer;
 		color: #ffffff;
@@ -40,18 +37,8 @@
 		font-size: 18px;
 		padding: 11px 16px;
 		text-decoration: none;
-		text-shadow: 1px 1px 0px #2f6627;
-	}
-
-	.myButton:hover {
-		background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #5cbf2a), color-stop(1, #44c767));
-		background: -moz-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
-		background: -webkit-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
-		background: -o-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
-		background: -ms-linear-gradient(top, #5cbf2a 5%, #44c767 100%);
-		background: linear-gradient(to bottom, #5cbf2a 5%, #44c767 100%);
-		filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#5cbf2a', endColorstr='#44c767', GradientType=0);
-		background-color: #5cbf2a;
+		text-shadow: 1px 1px 0px #c1a91f;
+		box-shadow: 5px 10px 8px 1px #888888;
 	}
 
 	.myButton:active {
--- src/components/ButtonPrevisualizar.vue
+++ src/components/ButtonPrevisualizar.vue
@@ -18,18 +18,18 @@
     }
     .myButton {
         display: inline-block;
-        background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #c7c745), color-stop(1, #9bbd2a));
-        background: -moz-linear-gradient(top, #c7c745 5%, #9bbd2a 100%);
-        background: -webkit-linear-gradient(top, #c7c745 5%, #9bbd2a 100%);
-        background: -o-linear-gradient(top, #c7c745 5%, #9bbd2a 100%);
-        background: -ms-linear-gradient(top, #c7c745 5%, #9bbd2a 100%);
-        background: linear-gradient(to bottom, #c7c745 5%, #9bbd2a 100%);
-        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#c7c745', endColorstr='#9bbd2a', GradientType=0);
-        background-color: #c7c745;
+        background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #637fb5), color-stop(1, #1D55BF));
+        background: -moz-linear-gradient(top, #637fb5 5%, #1D55BF 100%);
+        background: -webkit-linear-gradient(top, #637fb5 5%, #1D55BF 100%);
+        background: -o-linear-gradient(top, #637fb5 5%, #1D55BF 100%);
+        background: -ms-linear-gradient(top, #637fb5 5%, #1D55BF 100%);
+        background: linear-gradient(to bottom, #637fb5 5%, #1D55BF 100%);
+        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#637fb5', endColorstr='#1D55BF', GradientType=0);
+        background-color: #637fb5;
         -moz-border-radius: 5px 28px 28px 5px;
         -webkit-border-radius: 5px 28px 28px 5px;
         border-radius: 5px 28px 28px 5px;
-        border: 3px solid #9aab19;
+        border: 3px solid #1D55BF;
         display: inline-block;
         cursor: pointer;
         color: #ffffff;
@@ -37,18 +37,8 @@
         font-size: 18px;
         padding: 11px 16px;
         text-decoration: none;
-        text-shadow: 1px 1px 0px #5d6628;
-    }
-
-    .myButton:hover {
-        background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #9bbd2a), color-stop(1, #c7c745));
-        background: -moz-linear-gradient(top, #9bbd2a 5%, #c7c745 100%);
-        background: -webkit-linear-gradient(top, #9bbd2a 5%, #c7c745 100%);
-        background: -o-linear-gradient(top, #9bbd2a 5%, #c7c745 100%);
-        background: -ms-linear-gradient(top, #9bbd2a 5%, #c7c745 100%);
-        background: linear-gradient(to bottom, #9bbd2a 5%, #c7c745 100%);
-        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#9bbd2a', endColorstr='#c7c745', GradientType=0);
-        background-color: #9bbd2a;
+        text-shadow: 1px 1px 0px #637fb5;
+        box-shadow: 5px 10px 8px 1px #888888;
     }
 
     .myButton:active {
